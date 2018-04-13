d48eed08cdf4dd642bf014ed838b507ffec53787
misrab <faizullah.misrab@gmail.com>
2018-04-13 14:48:14 +0800

    starting database tdd

--- store/database_test.go
+++ store/database_test.go
@@ -2,8 +2,31 @@ package store
 
 import (
 	"testing"
+
+	"io/ioutil"
+)
+
+const (
+	TEST_DATA_URL = "../test_data/test.txt"
 )
 
 func TestNewDatabase(t *testing.T) {
+	_, err := NewDatabase()
+
+	if err != nil {
+		t.Fatalf("Could not open database: %v\n", err)
+	}
+}
+
+func TestDatabasePut(t *testing.T) {
+	db, _ := NewDatabase()
+
+	// open test file
+	file, err := ioutil.ReadFile(TEST_DATA_URL)
+	if err != nil {
+		t.Fatalf("Could not read file: %v\n", err)
+	}
+
+	db.Put(file)
 
 }
