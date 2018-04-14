8bef2d27aa59efc33fc646acede9a3d031795781
Misrab <faizullah.misrab@gmail.com>
2018-04-14 16:57:27 +0800

    store testing written, now need to work on Put with insert and retrieval encryption

--- store/database.go
+++ store/database.go
@@ -9,6 +9,8 @@ type Blob []byte
 type Database interface {
 	Get(multihash.Multihash) (Blob, error)
 	Put(Blob) (multihash.Multihash, error)
+
+	Remove(multihash.Multihash) error
 }
 
 func NewDatabase() (Database, error) {
@@ -25,5 +27,12 @@ func (db *database) Get(hash multihash.Multihash) (Blob, error) {
 }
 
 func (db *database) Put(Blob) (multihash.Multihash, error) {
+	// hash the file
+
 	return nil, nil
+}
+
+func (db *database) Remove(multihash.Multihash) error {
+	// TODO
+	return nil
 }
--- store/database_test.go
+++ store/database_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 
 	"io/ioutil"
+
+	"github.com/stretchr/testify/assert"
 )
 
 const (
@@ -18,6 +20,7 @@ func TestNewDatabase(t *testing.T) {
 	}
 }
 
+/*
 func TestDatabasePut(t *testing.T) {
 	db, _ := NewDatabase()
 
@@ -27,6 +30,33 @@ func TestDatabasePut(t *testing.T) {
 		t.Fatalf("Could not read file: %v\n", err)
 	}
 
-	db.Put(file)
+	_, err = db.Put(file)
+	if err != nil {
+		t.Fatalf("Could not put: %v\n", err)
+	}
+}
+*/
+func TestDatabasePutGetRemove(t *testing.T) {
+	db, _ := NewDatabase()
 
+	// insert test file
+	file, err := ioutil.ReadFile(TEST_DATA_URL)
+	if err != nil {
+		t.Fatalf("Could not read file: %v\n", err)
+	}
+
+	hash, err := db.Put(file)
+	if err != nil {
+		t.Fatalf("Could not put: %v\n", err)
+	}
+
+	// retrieve it
+	blob, err := db.Get(hash)
+	if err != nil {
+		t.Fatalf("Could not get: %v\n", err)
+	}
+	assert.Equal(t, file, blob, "The file inserted did not match the file retrieved")
+
+	// remove the file
+	// db.Remove(hash)
 }
